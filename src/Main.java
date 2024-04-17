0b80078bdf6b830b8b054dd0ae87b0968a2d0c9b
hugo de araujo <hugo.saraujo@tutanota.com>
2024-04-17 09:54:07 -0300

    Consumindo a API de livros do Google

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Main {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.print("Digite o título do livro: ");
+        var titulo = leitura.nextLine();
+
+        String chave = "AIzaSyCNCArQt98mjjrpc0Vrmfg1YbWafo78JBM";
+
+        String endereco = "https://www.googleapis.com/books/v1/volumes?q="+ titulo +"&key=" + chave;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
 }
